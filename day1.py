bbac914fe1e196f3d244b7a72737ca89228aecfc
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-03 19:52:59 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,15 @@
+print("this is the first program")
+
+#python filename.py command is used to run program on terminal
+
+#indentation code
+if (5>2): 
+    print("five is greater")
+else:
+    print("False")
+
+print(5//2) # floor division
+
+print(5/2) # normal division
+
+print(type("eight")) 
