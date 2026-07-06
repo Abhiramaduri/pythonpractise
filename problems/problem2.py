0ba3b74e05c7d1d22306d981361d6beeb0efaa07
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-06 22:55:35 +0530

    prblm2

---
+++ problems/problem2.py
@@ -0,0 +1,18 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+a = 'Thirty'
+b= ' Days'
+c= ' Of'
+d = ' Python'
+
+print(a+b + c + d)
+
+#Print the length of the company string using len() method and print().
+c = 'company'
+print(len(c))
+
+#Change all the characters to uppercase letters using upper() method.
+
+print(c.upper())
+
+u= 'UPPER'
+print(u.lower())
