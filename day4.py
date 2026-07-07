01ecda98cab29d512ad09716ed74887b04fffefd
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-07 21:53:45 +0530

    Fourth day

---
+++ day4.py
@@ -0,0 +1,17 @@
+# String concantenation
+
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+a = 'Thirty'
+b = ' Days'
+c =  ' Of'
+d = ' Python'
+
+print(a + b + c + d)
+
+#Declare a variable named company and assign it to an initial value "Coding For All".
+
+company = 'Coding For All'
+#Print the variable company using print().
+
+print(company)
+
