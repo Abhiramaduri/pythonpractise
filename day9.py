794307f1797fb342bd68367377d2c7164e834b4c
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-11 22:19:38 +0530

    ninth day

--- day9.py
+++ day9.py
@@ -13,3 +13,19 @@ def convert_celsius_to_fahrenheit(degree):
     f=(degree*9/5)+32
     print(f)
 convert_celsius_to_fahrenheit(15)  #function call
+
+#Area of a circle is calculated as follows: area = π x r x r. Write a function that calculates area_of_circle.
+
+def circle_area(r):
+    pie = 3.14
+    area = pie*r*r
+    return area
+print(circle_area(12))
+
+#Declare a function named print_list. It takes a list as a parameter and it prints out each element of the list.
+
+def print_list(s):
+    for i in s:
+        print(i)
+arr=['b','v','d']
+print_list(arr)
