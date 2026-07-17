38973e41de7af577bbc76508a9faf6137d7defaa
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-17 23:56:21 +0530

    Day thirteen

--- day13.py
+++ day13.py
@@ -5,3 +5,11 @@ def change_upper(x):
     return x.upper()
 result=map(change_upper,countries)
 print(list(result))
+
+#Use map to create a new list by changing each number to its square in the numbers list
+
+numbers = [1, 2, 3, 4, 5]
+
+squared_numbers = list(map(lambda num: num ** 2, numbers))
+
+print(squared_numbers)
