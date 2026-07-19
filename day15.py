b8af48aa34e7c86774cfc29b5abee67be740c41c
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-19 11:51:28 +0530

    15th day

---
+++ day15.py
@@ -0,0 +1,8 @@
+#zero diviision error prblm
+
+try:
+    y=int(input("enter number"))
+    result= y/0
+    print(result)
+except:
+    print("Zero division error")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,4 @@
-age = 21
-height = 5.10
-#Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
-base = float(input("enter base"))
-height= float(input("enter height"))
-area = 0.5 *base *height
-print("Area of triangle is ", area)
+for i in range(0,100):
+    for j in range(i,100):
+        print(i)
+        print(j)
