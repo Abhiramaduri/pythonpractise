7dccecda69be1fc9d0d231ccd2ee4ff38986c8b2
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-20 22:02:05 +0530

    day 16

---
+++ day16.py
@@ -0,0 +1,19 @@
+# Even and odd number using lambda function
+
+oddeven = lambda a:  a%2==0 
+print(oddeven(8))
+
+# cube of a number using lambda
+
+cubevalue= lambda x: x*x*x
+print(cubevalue(3))
+
+# take 3 numbers and multiply them using lambda function
+
+multiples = lambda x,y,z: x*y*z
+print(multiples(2,3,5))
+
+# take x,y and return the exponential value of both numbers using lambda
+
+result= lambda a,b : a**b
+print(result(10,20))   
