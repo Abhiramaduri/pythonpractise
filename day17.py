98a635e855717b9f92280ddc8f274f9fc80e02a1
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-23 23:32:27 +0530

    day-17

--- day17.py
+++ day17.py
@@ -117,3 +117,26 @@ print(most_spoken_languages("./data/countries_data.json", 3))
 #]
 
 #Extract all incoming email addresses as a list from the email_exchange_big.txt file.
+
+#Find the most common words in the English language. Call the name of your function find_most_common_words, it will take two parameters - a string or a file and a positive integer, indicating the number of words. Your function will return an array of tuples in descending order. Check the output
+    # Your output should look like this
+    #print(find_most_common_words('sample.txt', 10))
+    #[(10, 'the'),
+    #(8, 'be'),
+    #(6, 'to'),
+    #(6, 'of'),
+    #(5, 'and'),
+    #(4, 'a'),
+    #(4, 'in'),
+    #(3, 'that'),
+    #(2, 'have'),
+    #(2, 'I')]
+
+    # Your output should look like this
+    #print(find_most_common_words('sample.txt', 5))
+
+    #[(10, 'the'),
+    #(8, 'be'),
+    #(6, 'to'),
+    #(6, 'of'),
+    #(5, 'and')]
