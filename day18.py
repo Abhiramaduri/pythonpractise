703fc517ecd13a7c2b74cc5fcaf046417e28744e
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-24 23:32:20 +0530

    day-18

--- day18.py
+++ day18.py
@@ -12,3 +12,16 @@
 #print('Standard Deviation: ', data.std()) # 4.2
 #print('Variance: ', data.var()) # 17.5
 #print('Frequency Distribution: ', data.freq_dist()) # [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+# you output should look like this
+#print(data.describe())
+#Count: 25
+#Sum:  744
+#Min:  24
+#Max:  38
+#Range:  14
+#Mean:  30
+#Median:  29
+#Mode:  (26, 5)
+#Variance:  17.5
+#Standard Deviation:  4.2
+#Frequency Distribution: [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
