d646025415734173c9537f75def86415375fe3c2
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-25 23:54:40 +0530

    day-18

--- day18.py
+++ day18.py
@@ -25,4 +25,42 @@
 #Variance:  17.5
 #Standard Deviation:  4.2
 #Frequency Distribution: [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+
 #Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has total_income, total_expense, account_info, add_income, add_expense and account_balance methods. Incomes is a set of incomes and its description. The same goes for expenses.
+
+class PersonAccount:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.incomes = set()
+        self.expenses = set()
+
+    def add_income(self, amount, description):
+        self.incomes.add((amount, description))
+
+    def add_expense(self, amount, description):
+        self.expenses.add((amount, description))
+
+    def total_income(self):
+        return sum(amount for amount, _ in self.incomes)
+
+    def total_expense(self):
+        return sum(amount for amount, _ in self.expenses)
+
+    def account_info(self):
+        return f"First name: {self.firstname}\nLast name: {self.lastname}\nIncomes: {self.incomes}\nExpenses: {self.expenses}"
+
+    def account_balance(self):
+        return self.total_income() - self.total_expense()
+
+
+account = PersonAccount('Asabeneh', 'Yetayeh')
+account.add_income(1000, 'Salary')
+account.add_income(500, 'Freelance work')
+account.add_expense(200, 'Rent')
+account.add_expense(100, 'Food')
+
+print('Total Income:', account.total_income())
+print('Total Expense:', account.total_expense())
+print('Account Balance:', account.account_balance())
+print(account.account_info())
