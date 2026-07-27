4a53f163ac7193926c9f320d9a515b6f30709311
Abhiramaduri <aduriabhiram1234@gmail.com>
2026-07-27 23:46:09 +0530

    day-20

--- day20.py
+++ day20.py
@@ -8,7 +8,17 @@
 #         print("*",end=" ")
 #     print()
 
-for i in range(1, 6):
-    for j in range(1, i + 1):
-        print(j, end="")
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         print(j, end="")
+#     print()
+
+# for i in range (6):
+#     for j in range(1,i+1):
+#         print(i,end="")
+#     print()
+
+for i in range (5):
+    for j in range(5-i):
+        print("*", end="")
     print()
